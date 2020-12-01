ba1f0279858a737647297b935f075e74b9726ac0
Croquelois <croquelois.adrien@gmail.com>
2020-12-02 00:50:54 +0100

    :sparkles: add deindexing

--- functions/index.js
+++ functions/index.js
@@ -10,14 +10,17 @@ function getQuestionIdFromPayload(payload) {
   const {
     event: {
       data: {
-        new: { Id: questionId },
+        new: { Id, IsPublie },
       },
     },
   } = payload
-  return questionId
+  return {
+    Id: Id,
+    IsPublie: IsPublie,
+  }
 }
 
-function buildBody(id) {
+function buildGetBody(id) {
   return `{
   "query":
     "query MyQuery {
@@ -43,10 +46,22 @@ function buildBody(id) {
 `
 }
 
+function buildDeleteBody(questionId) {
+  return `{
+  "query": {
+      "term": {
+        "Id": {
+          "value": ${questionId}
+          }
+        }
+  }
+}`
+}
+
 async function getQuestionByPk(questionId) {
   const clientServerOptions = {
     uri: `${process.env.HASURA_SERV_URL}/v1/graphql`,
-    body: buildBody(questionId),
+    body: buildGetBody(questionId),
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -86,20 +101,52 @@ async function indexQuestion(questionId) {
   })
 }
 
+async function removeIndexQuestion(questionId) {
+  const clientServerOptions = {
+    uri: `${process.env.ELASTIC_SERV_URL}/questions/_delete_by_query`,
+    body: await buildDeleteBody(questionId),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  return new Promise(function (resolve, reject) {
+    request(clientServerOptions, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(body)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
+
 app.use(bodyParser.json())
 
 app.post('/index-question', async function (req, res) {
   try {
-    const questionId = getQuestionIdFromPayload(req.body)
-    indexQuestion(questionId)
-      .then(() => {
-        console.log(`[INFO]: Question ${questionId} was indexed`)
-        res.status(201).json({ msg: 'Question indexation success' })
-      })
-      .catch(() => {
-        console.log(`[WARNING]: Indexation failed for question: ${questionId}`)
-        res.status(500).json({ msg: 'Question indexation failed' })
-      })
+    const { Id, IsPublie } = getQuestionIdFromPayload(req.body)
+    if (IsPublie) {
+      indexQuestion(Id)
+        .then(() => {
+          console.log(`[INFO]: Question ${Id} was indexed`)
+          res.status(200).json({ msg: 'Question indexation success' })
+        })
+        .catch(() => {
+          console.log(`[WARNING]: Indexation failed for question: ${Id}`)
+          res.status(500).json({ msg: 'Question indexation failed' })
+        })
+    } else {
+      removeIndexQuestion(Id)
+        .then(() => {
+          console.log(`[INFO]: Question ${Id} was deindexed`)
+          res.status(200).json({ msg: 'Question deindexed success' })
+        })
+        .catch(() => {
+          console.log(`[WARNING]: Indexation failed for question: ${Id}`)
+          res.status(500).json({ msg: 'Question deindexed failed' })
+        })
+    }
   } catch (e) {
     console.log(`[ERROR]: Error in the payload`)
     res.status(500).json({ msg: 'Error in the payload' })
