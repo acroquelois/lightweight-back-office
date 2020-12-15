e87bfcee402a1e3bb8cfb69212db30c7ea551f50
Croquelois <croquelois.adrien@gmail.com>
2020-12-16 00:07:57 +0100

    :wrench: add credential

---
+++ functions/src/config.ts
@@ -0,0 +1,10 @@
+// TODO: env variable
+export const config = {
+  apiKey: 'AIzaSyDSMhG08KUC4jCcFC3UbajkmERCeFsSlMY',
+  authDomain: 'lightweight-back-office.firebaseapp.com',
+  databaseURL: 'https://lightweight-back-office.firebaseio.com',
+  projectId: 'lightweight-back-office',
+  storageBucket: 'lightweight-back-office.appspot.com',
+  messagingSenderId: '603179471698',
+  appId: '1:603179471698:web:cd76bfc2e3700d9082a08b',
+}
